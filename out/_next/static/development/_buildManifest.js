373d5b54af3616bbeb84cd34b1d99bf1f4f37c47
Manuel Goulao <msilvagoulao@gmail.com>
2020-08-14 13:38:04 +0100

    Deploy website to gh-pages

--- out/_next/static/development/_buildManifest.js
+++ out/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/content/[topic]/[page]":["static\u002Fchunks\u002Fpages\u002Fcontent\u002F[topic]\u002F[page].js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
