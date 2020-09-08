55b4c02b643338490e9a4f86d16097bd4914c866
Manuel Goulao <msilvagoulao@gmail.com>
2020-09-08 11:42:13 +0100

    add key press listener to search input, closes #6

--- components/headerSearch.tsx
+++ components/headerSearch.tsx
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import { useRouter } from 'next/router'
 
 import styles from "./layout.module.css";
 
 export default function HeaderSearch() {
   const [query, setQuery] = useState("");
+  const router = useRouter();
 
   const inputChange = (event) => {
     setQuery(event.target.value);
   };
 
+  const keyPressHandler = (event) => {
+    if (event.key === "Enter")
+      router.push(`${process.env.BACKEND_URL}/search?query=${query}`);
+  };
+
   return (
     <div className={styles.headerSearch}>
-      <input placeholder="Search" onChange={inputChange}></input>
+      <input placeholder="Search" onChange={inputChange} onKeyPress={keyPressHandler}></input>
       <Link href={{ pathname: `${process.env.BACKEND_URL}/search`, query: { query: query } }}>
         <a className={styles.headerSearchBtn}>
           <img src={`${process.env.BACKEND_URL}/icons/search.svg`}></img>
