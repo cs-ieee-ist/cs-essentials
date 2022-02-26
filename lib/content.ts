810ec10bc2942af164c6d8d37feb7048b2da1f15
Manuel Goulão <msilvagoulao@gmail.com>
2022-02-26 13:33:07 +0000

    update remark

--- lib/content.ts
+++ lib/content.ts
@@ -1,9 +1,12 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import remark from 'remark'
-import html from 'remark-html'
-import highlight from 'remark-highlight.js'
+import {unified} from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
+import rehypeHighlight from 'rehype-highlight'
+import remarkGfm from 'remark-gfm'
 
 const rootFilesNames = {
 	ABOUT: "about.md",
@@ -83,10 +86,13 @@ export async function getContentData(topic: string, page: string) {
 	const matterResult = matter(fileContents)
 
 	// Use remark to convert markdown into HTML string
-	const processedContent = await remark()
-		.use(html)
-		.use(highlight)
-		.process(matterResult.content)
+	const processedContent = await unified()
+	.use(remarkParse)
+	.use(remarkGfm)
+	.use(rehypeHighlight)
+	.use(remarkRehype)
+	.use(rehypeStringify)
+	.process(matterResult.content)
 	const contentHtml = processedContent.toString()
 
 	// Combine the data with the id and contentHtml
@@ -115,8 +121,12 @@ export async function getPageContent(fileName: string) {
 	const matterResult = matter(fileContents)
 
 	// Use remark to convert markdown into HTML string
-	const processedContent = await remark()
-		.use(html)
+	const processedContent = await unified()
+		.use(remarkParse)
+		.use(remarkGfm)
+		.use(remarkRehype)
+		.use(rehypeHighlight)
+		.use(rehypeStringify)
 		.process(matterResult.content)
 	const contentHtml = processedContent.toString()
 
